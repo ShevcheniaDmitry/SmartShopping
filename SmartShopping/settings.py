caed7ae8a253edb483d04b7538ebe5b353811717
Shevchenia Dmitry <shevchenia@gmail.com>
2021-07-06 07:29:20 +0300

    сhanged

--- SmartShopping/settings.py
+++ SmartShopping/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-s3ji$9(904h6njx57n$fyn+%1y&m9n3#)kk_dq=y@jd4o+ezlk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
